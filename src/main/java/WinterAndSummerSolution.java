1599a949257e25ba666ef8f78d9115c7344a1c52
vandreyd1 <vandreyd1@gmail.com>
2018-06-24 21:11:19 +0300

    solution logic updated

--- src/main/java/WinterAndSummerSolution.java
+++ src/main/java/WinterAndSummerSolution.java
@@ -1,12 +1,30 @@
+import java.util.Arrays;
+
 public class WinterAndSummerSolution {
 
-    public static int solution(int[] T){
-        int maxWint = T[0];
+    public static int solution(int[] T) {
+        int maxTemp = T[0];
         for (int i = 1; i < T.length; i++) {
-            if (T[i+1] > maxWint){
-                return i+1;
+            if (T[i] > maxTemp) {
+                int avg = avgT(Arrays.copyOf(T, i+1),  i+1);
+                int avgTempNext = avgT(Arrays.copyOf(T, i + 2), i + 1);
+                {
+                    if (avgTempNext > avg) {
+                        return i;
+                    } else {
+                        maxTemp = T[i];
+                    }
+                }
             }
         }
-        return 0;
+        return 1;
+    }
+
+    private static int avgT(int[] T, int count) {
+        int sumElem = 0;
+        for (int i = 0; i < T.length; i++) {
+            sumElem += T[i];
+        }
+        return sumElem / (count);
     }
 }
--- src/test/java/WinterAndSummerTest.java
+++ src/test/java/WinterAndSummerTest.java
@@ -3,6 +3,9 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @RunWith(JUnit4.class)
 public class WinterAndSummerTest {
     @Test
@@ -11,5 +14,9 @@ public class WinterAndSummerTest {
         Assert.assertEquals(3,WinterAndSummerSolution.solution(T1));
         int[] T2 = {-5, -5, -5, -42, 6, 12};
         Assert.assertEquals(4,WinterAndSummerSolution.solution(T2));
+        int[] T3 = {-5, -5, 4, -42, 6, 12};
+        Assert.assertEquals(4,WinterAndSummerSolution.solution(T3));
+        int[] T4 = {-5,25};
+        Assert.assertEquals(1,WinterAndSummerSolution.solution(T4));
     }
 }
