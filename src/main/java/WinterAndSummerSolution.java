39dd9d006eaed12ab2fb9a736de09672d5d1bdea
vandreyd1 <vandreyd1@gmail.com>
2018-06-25 19:02:50 +0300

    solution optimization

--- src/main/java/WinterAndSummerSolution.java
+++ src/main/java/WinterAndSummerSolution.java
@@ -1,15 +1,11 @@
-import java.util.Arrays;
-
 public class WinterAndSummerSolution {
 
     public static int solution(int[] T) {
         int maxTemp = T[0];
-        for (int i = 1; i < T.length; i++) {
-            if (T[i] > maxTemp) {
-                int avg = avgT(Arrays.copyOf(T, i+1),  i+1);
-                int avgTempNext = avgT(Arrays.copyOf(T, i + 2), i + 1);
-                {
-                    if (avgTempNext > avg) {
+        if (T.length >= 3) {
+            for (int i = 0; i < T.length; i++) {
+                if (T[i] > maxTemp) {
+                    if (T[i + 1] > maxTemp) {
                         return i;
                     } else {
                         maxTemp = T[i];
@@ -18,13 +14,5 @@ public class WinterAndSummerSolution {
             }
         }
         return 1;
-    }
-
-    private static int avgT(int[] T, int count) {
-        int sumElem = 0;
-        for (int i = 0; i < T.length; i++) {
-            sumElem += T[i];
-        }
-        return sumElem / (count);
     }
 }
--- src/test/java/WinterAndSummerTest.java
+++ src/test/java/WinterAndSummerTest.java
@@ -3,20 +3,18 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
 
-import java.util.ArrayList;
-import java.util.List;
 
 @RunWith(JUnit4.class)
 public class WinterAndSummerTest {
     @Test
-    public void solutionTest(){
+    public void solutionTest() {
         int[] T1 = {5, -2, 3, 8, 6};
-        Assert.assertEquals(3,WinterAndSummerSolution.solution(T1));
+        Assert.assertEquals(3, WinterAndSummerSolution.solution(T1));
         int[] T2 = {-5, -5, -5, -42, 6, 12};
-        Assert.assertEquals(4,WinterAndSummerSolution.solution(T2));
+        Assert.assertEquals(4, WinterAndSummerSolution.solution(T2));
         int[] T3 = {-5, -5, 4, -42, 6, 12};
-        Assert.assertEquals(4,WinterAndSummerSolution.solution(T3));
-        int[] T4 = {-5,25};
-        Assert.assertEquals(1,WinterAndSummerSolution.solution(T4));
+        Assert.assertEquals(4, WinterAndSummerSolution.solution(T3));
+        int[] T4 = {-5, 25};
+        Assert.assertEquals(1, WinterAndSummerSolution.solution(T4));
     }
 }
